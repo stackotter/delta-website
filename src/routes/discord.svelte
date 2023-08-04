21294a8bb3c3490a4108ebc7b33e301a70e4c223
stackotter <stackotter@stackotter.dev>
2023-08-04 10:10:03 +1000

    Update invite link

--- src/routes/discord.svelte
+++ src/routes/discord.svelte
@@ -1,8 +1,8 @@
 <script context="module">
-  export async function load() {
-    return {
-      status: 302,
-      redirect: "https://discord.gg/ZmDNjQfdr5"
-    };
-  }
+	export async function load() {
+		return {
+			status: 302,
+			redirect: 'https://discord.gg/up7F3tFmkZ'
+		};
+	}
 </script>
